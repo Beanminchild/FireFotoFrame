8b48f49133a071408cd9608624f1affcf2106dd3
Zach <zacharyend@proton.me>
2025-11-26 23:40:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 (function() {
   const urls = [
     "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;E79A6A02-A0A5-4D5B-BEED-E466B2290865",
-    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx",
+    "https://www.icloud.com/sharedalbum/#B1z59UlCqrHT7vf;B03A76C0-A83E-4BF6-A556-4816F61E0FAE",
     
     "https://duckduckgo.com/?q=weather+in+plum+pa&atb=v481-1&ia=web"
     
