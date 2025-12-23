63c81156b5de1d92276fcbc50964938a8ad8c9fb
Zach <zacharyend@proton.me>
2025-12-23 20:16:41 +0000

    final touches on frame code

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 
 (function() {
   const urls = [
-    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;E79A6A02-A0A5-4D5B-BEED-E466B2290865",
+
     "https://beanminchild.github.io/FireFotoFrame/christmasClock",
-    "https://www.icloud.com/sharedalbum/#B1z59UlCqrHT7vf;B03A76C0-A83E-4BF6-A556-4816F61E0FAE",
-    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;ECB02A21-237B-42C3-AA1F-55A85FCB5364",
-    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;D648FB96-72DD-48C8-B117-764E9BEBF74D",
-    "https://beanminchild.github.io/FireFotoFrame/christmasClock",
-    "https://www.icloud.com/sharedalbum/#B1zG4TcsmqOoZFQ;8466AF58-15BE-48F9-9F78-5E460F44522D",
-    "https://www.icloud.com/sharedalbum/#B1zG4TcsmqOoZFQ;466E890E-C9EF-4352-8398-33856093337E",
-    "https://www.icloud.com/sharedalbum/#B1z5qXGF1r4LPMn;4AEA6C3A-2CDA-4159-B38B-DC1848CD3966",
-    "https://www.icloud.com/sharedalbum/#B1zG4TcsmqOoZFQ;1B17276D-3239-4340-9FF7-CB19115B104D",
-    "https://www.icloud.com/sharedalbum/#B1zGfnH8tqM1vul;B84879FC-40C2-4AB7-AEEB-265F840FBBE3",
-    "https://www.icloud.com/sharedalbum/#B1zGfnH8tqM1vul;8598FF63-F82C-46B9-8E5B-4837D6D36768",
-    "https://www.icloud.com/sharedalbum/#B1zGrq0zwqrpHfU;4B99F8C2-4CA1-4FB7-8BBC-D4D7B26E2622",
-    "https://www.icloud.com/sharedalbum/#B1zGrq0zwqrpHfU;780E8127-8BEE-4967-A772-758E08BBD083",
-    "https://www.icloud.com/sharedalbum/#B1z59UlCqrHT7vf;FE5A2DD5-745B-4060-9976-088C05255F3B",
-    "https://www.icloud.com/sharedalbum/#B1zGrq0zwqrpHfU;B99281AA-6230-49E6-A36D-2562F408E6E3",
-    "https://beanminchild.github.io/FireFotoFrame/christmasClock",
+    "https://duckduckgo.com/?q=weather+in+plum+pa&atb=v481-1&ia=web",
+    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;838712E6-32BA-4E77-8DDD-788F7B8DCC6E", //DGR3
+    "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;8A7A530D-5F4B-4087-9A62-9A628764E16B", //DGR3
+    "https://www.icloud.com/sharedalbum/#B1z59UlCqrHT7vf;155BA3C5-80F4-4208-9BF8-927FFC284689", //CCR
+    "https://www.icloud.com/sharedalbum/#B2HG4TcsmGRbn4W;1759FBD5-37FD-4436-9497-B5F516BFB245", //bestofgorlie
+    "https://www.icloud.com/sharedalbum/#B1zGfnH8tqM1vul;85F12A9F-94FC-4BFD-B970-DF4174D986B1", //Deepcreek2025
+    "https://www.icloud.com/sharedalbum/#B1zGrq0zwqrpHfU;C4BB296F-AA12-441D-BCA5-917636068EBA", //Digital repo part II
+    "https://www.icloud.com/sharedalbum/#B1zGqkRUiqraMoC;42AB7D37-9CF3-4476-BF69-819F47768A49", //Boston Trip
+    "https://www.icloud.com/sharedalbum/#B1z5ON9t3rYEe9K;E72C81DD-B9DC-41B9-B8AF-F45E1496C4E0", //lake trip
+    "https://www.icloud.com/sharedalbum/#B1zJ0DiRHPTdtH7;433CC241-1FCC-4D87-B23F-E338FA2CB231", // ohiopile
+    "https://www.icloud.com/sharedalbum/#B1zG4TcsmqOoZFQ;ADF0FA87-0483-4D40-B13C-9E927DF1E440", //DGR1
+    "https://www.icloud.com/sharedalbum/#B1x5M7GFPJRnk4P;90AAD6BE-1D1E-4C7A-BC93-E086E71588F2", //WHO
+
+
+
 
     
-    "https://duckduckgo.com/?q=weather+in+plum+pa&atb=v481-1&ia=web",
+
+
 
     
   ];
