7a57d3863cb8a74e73a1b04ed0def7990148c09e
Zach <zacharyend@proton.me>
2025-12-31 02:15:39 +0000

    New Years Clock Update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 (function() {
   const urls = [
 
-    "https://beanminchild.github.io/FireFotoFrame/christmasClock",
+    "https://beanminchild.github.io/FireFotoFrame/newYearsClock",
     "https://duckduckgo.com/?q=weather+in+plum+pa&atb=v481-1&ia=web",
     "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;838712E6-32BA-4E77-8DDD-788F7B8DCC6E", //DGR3
     "https://www.icloud.com/sharedalbum/#B1z53qWtHqA2mdx;8C2DB22B-831E-49C3-9D4B-EDD717193EE4", //DGR3
@@ -12,14 +12,16 @@
     "https://www.icloud.com/sharedalbum/#B1zGrq0zwqrpHfU;C4BB296F-AA12-441D-BCA5-917636068EBA", //Digital repo part II
     "https://www.icloud.com/sharedalbum/#B1zGqkRUiqraMoC;42AB7D37-9CF3-4476-BF69-819F47768A49", //Boston Trip
     "https://www.icloud.com/sharedalbum/#B1z5ON9t3rYEe9K;E72C81DD-B9DC-41B9-B8AF-F45E1496C4E0", //lake trip
+    "https://beanminchild.github.io/FireFotoFrame/newYearsClock",
     
     "https://www.icloud.com/sharedalbum/#B1zG4TcsmqOoZFQ;ADF0FA87-0483-4D40-B13C-9E927DF1E440", //DGR1
     "https://www.icloud.com/sharedalbum/#B1x5M7GFPJRnk4P;90AAD6BE-1D1E-4C7A-BC93-E086E71588F2", //WHO
     "https://www.icloud.com/sharedalbum/#B1z5qXGF1r4LPMn;64CA806E-65A0-4065-9559-29733E1F7F27", //end family thanksgiving part II
     "https://www.icloud.com/sharedalbum/#B1z5nhQSTrRUltf;BDD03EA0-7FC7-4864-A069-59D7E510F299", //bidges baptims
+    "https://beanminchild.github.io/FireFotoFrame/newYearsClock",
     "https://www.icloud.com/sharedalbum/#B1zGWZuqDqPg0LW;8DE426BD-C27F-462C-ABB2-B3F015D6AC4D", //the gang attempts a family vacation
     "https://www.icloud.com/sharedalbum/#B1zGY8gBYq8xewJ;7E00DE24-A3E6-4488-98AD-FA74F7DDC71E", // bidges adoption zacgs version
-    "https://beanminchild.github.io/FireFotoFrame/christmasClock"
+    "https://beanminchild.github.io/FireFotoFrame/newYearsClock"
 
 
 
